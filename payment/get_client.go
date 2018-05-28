60639f521902d781ea9cfc23b0261fc0b62c6bd3
VanThanh1812 <phanvanthanh.mrt@gmail.com>
2018-05-28 09:25:55 +0700

    dev

--- payment/get_client.go
+++ payment/get_client.go
@@ -11,7 +11,11 @@ const (
 	addrContract = "0xdccc2dd10c7b3be9661aecf87b99f65f290c3f1f"
 	addrOwner = "0x3A120D99ac41D6e0630382752F9714D8772c5432"
 	// import account to unlock in geth, file keyjson in keystore
-	key = `{"address":"3a120d99ac41d6e0630382752f9714d8772c5432","crypto":{"cipher":"aes-128-ctr","ciphertext":"eca3d8f8eb49b969eb17ec58b486cf4d86162cac6007f9a52aedc7c616c14830","cipherparams":{"iv":"475a6ef6eb8ca5a546b78a733c6a7bc9"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"960c0080b60884d1b9c7cbb1e655025da4fb1f9420ef945e77368ca9ff25f285"},"mac":"cb2b4a000e9ec1b3939b60cf9f92472b90bc72379830635c4a3b491234ca3916"},"id":"45185a27-5e40-4839-81f5-b052b0d487d2","version":3}`
+	key = `{"address":"3a120d99ac41d6e0630382752f9714d8772c5432","crypto":{"cipher":"aes-128-ctr","ciphertext":"a844aa4d9aea20
+0f8b914b8b55f7301a789dd306cefbe9770b114aa2949bfa9d","cipherparams":{"iv":"f488e7f5c5de84f18f9978280e428aa1"},"kdf":
+"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"ad69abe52dfa549b5d6c444cef3da08a8167e2f639c4e1d0d68
+a591039d205ed"},"mac":"690b5c57a539a5074abae6f87dd8d1094d6f76c52be971286e0698550d24d77d"},"id":"be3303af-83c3-4555-
+a10b-1bcf4920b659","version":3}`
 	//pathIPC = "/home/vanthanhbk/.ethereum/rinkeby/geth.ipc"
 	pathIPC = "/home/phanvanthanh_mrt/.ethereum/rinkeby/geth.ipc"
 )
