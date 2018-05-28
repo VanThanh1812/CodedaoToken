bd010aada762351de9ab1bf44d1d9ca471f43650
VanThanh1812 <phanvanthanh.mrt@gmail.com>
2018-05-28 09:56:07 +0700

    dev

--- payment/transfer.go
+++ payment/transfer.go
@@ -10,6 +10,7 @@ import (
 	"codedaotoken/models"
 	"github.com/ethereum/go-ethereum/core/types"
 	"errors"
+	"regexp"
 )
 
 var (
@@ -21,10 +22,23 @@ func init(){
 	auth, _ = bind.NewTransactor(strings.NewReader(key), "thanhpv1234")
 }
 
+func checkAddress(addr string) bool {
+	match, _ := regexp.MatchString("^0x[a-fA-F0-9]{40}$", addr)
+	return match
+}
+
 func Transfer(proofkey string, rq models.TransferRequest)(*types.Transaction, error){
 
 	if !checkProofKey(proofkey) {
 		return nil, errors.New("Proof key error.")
+	}
+
+	if rq.Amount <= 0 {
+		return nil, errors.New("Amount require more than 0")
+	}
+
+	if checkAddress(rq.AddrTo){
+		return nil, errors.New("Address To wrong.")
 	}
 
 	tx, err := tokenContract.Transfer(auth, common.HexToAddress(rq.AddrTo), big.NewInt(rq.Amount))
@@ -41,6 +55,18 @@ func TransferFrom(proofkey string, rq models.TransferFromRequest)(*types.Transac
 		return nil, errors.New("Proof key error.")
 	}
 
+	if checkAddress(rq.AddrTo){
+		return nil, errors.New("Address To wrong.")
+	}
+
+	if checkAddress(rq.AddrFrom){
+		return nil, errors.New("Address From wrong.")
+	}
+
+	if rq.Amount <= 0 {
+		return nil, errors.New("Amount require more than 0")
+	}
+
 	tx, err := tokenContract.TransferFrom(auth, common.HexToAddress(rq.AddrFrom),common.HexToAddress(rq.AddrTo), big.NewInt(rq.Amount))
 	if err != nil {
 		log.Fatalf("Failed to request token transfer: %v", err)
@@ -53,6 +79,14 @@ func TransferFrom(proofkey string, rq models.TransferFromRequest)(*types.Transac
 func Earn(proofkey string, eq models.EarnRequest)(*types.Transaction, error){
 	if !checkProofKey(proofkey) {
 		return nil, errors.New("Proof key error.")
+	}
+
+	if checkAddress(eq.AddrTo){
+		return nil, errors.New("Address To wrong.")
+	}
+
+	if eq.Amount <= 0 {
+		return nil, errors.New("Amount require more than 0")
 	}
 
 	tx, err := tokenContract.Earn(auth, common.HexToAddress(eq.AddrTo), big.NewInt(eq.Amount))
